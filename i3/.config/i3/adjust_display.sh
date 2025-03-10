120e2fd645f419e8ade347dd0fad2efdbf6dcbbb
arch <arch@archlinux.org>
2025-03-10 20:09:52 +0800

    number six commit

--- i3/.config/i3/adjust_display.sh
+++ i3/.config/i3/adjust_display.sh
@@ -10,14 +10,14 @@ case "$RESOLUTION" in
   xrdb -merge <<<"Xft.dpi: 192"
   # export GDK_SCALE=2
   # export GDK_DPI_SCALE=2
-  # export QT_SCALE_FACTOR=2
-  feh --bg-scale $HOME/repo/wallpaper/3840x2160/wallhaven-zye7oj_3840x2160.png
+  export QT_SCALE_FACTOR=2
+  feh --bg-scale $HOME/repo/wallpaper/3840x2160/wallhaven-39yr8y_3840x2160.png
   ;;
 "1920x1080")
   # 1080p 分辨率，设置 DPI 150%
   xrandr --output HDMI-1 --mode 1920x1080
   xrdb -merge <<<"Xft.dpi: 144"
-  feh --bg-scale $HOME/repo/wallpaper/1920×1080/wallhaven-gpgj13_1920x1080.png
+  feh --bg-scale $HOME/repo/wallpaper/1920×1080/wallhaven-9dyvok_1920x1080.png
   ;;
 *)
   # "未匹配到特定分辨率，使用默认设置"
