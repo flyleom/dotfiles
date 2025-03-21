2adf2485a634ce7760f8bd0e4039c062d0443de3
arch <arch@archlinux.org>
2025-03-22 00:40:30 +0800

    number twelve commit

--- kitty/.config/kitty/launch.sh
+++ kitty/.config/kitty/launch.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 # 获取主显示器的分辨率
-RESOLUTION=$(xrandr | grep '*' | awk '{print $1}')
+# RESOLUTION=$(xrandr | grep '*' | awk '{print $1}')
+
+RESOLUTION=$(hyprctl monitors | awk '/^[[:space:]]*[0-9]+x[0-9]+@/ {print $1; exit}' | cut -d'@' -f1)
 
 if [ "$RESOLUTION" = "3840x2160" ]; then
   CONFIG_FILE=$HOME/.config/kitty/kitty_4k.conf
